e30dce0c83dba1a92a5c2c6c040a1a3e24afc41f
chenzhe142 <chenzhe142@163.com>
2015-02-04 21:17:11 -0600

    Feb.4
    Movie player function available.

--- Course-499Project/ViewController.swift
+++ Course-499Project/ViewController.swift
@@ -7,19 +7,58 @@
 //
 
 import UIKit
+import MediaPlayer
 
 class ViewController: UIViewController {
 
+    var moviePlayer:MPMoviePlayerController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        if var filePath = NSBundle.mainBundle().pathForResource("test", ofType: "mp4") {
+            var filePathUrl = NSURL.fileURLWithPath(filePath)
+            moviePlayer = MPMoviePlayerController(contentURL: filePathUrl)
+        } else {
+            var url:NSURL = NSURL(string: "http://jplayer.org/video/m4v/Big_Buck_Bunny_Trailer.m4v")!
+            moviePlayer = MPMoviePlayerController(contentURL: url)
+        }
+        
+        
+        // 'Exit Fullscreen' listener
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "onMPMoviePlayerWillExitFullscreenNotificationReceived:", name: MPMoviePlayerWillExitFullscreenNotification, object: nil)
+
+        // 'Did Finish' listener
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "onMPMoviePlayerPlaybackDidFinishNotificationReceived:", name: MPMoviePlayerPlaybackDidFinishNotification, object: nil)
     }
+    
+    // work with NotificationCenter
+    func onMPMoviePlayerWillExitFullscreenNotificationReceived(notification: NSNotification){
+        println("onMPMoviePlayerWillExitFullscreenNotificationReceived")
+        self.moviePlayer.view.removeFromSuperview()
+    }
+    
+    // work with NotificationCenter
+    func onMPMoviePlayerPlaybackDidFinishNotificationReceived(notification: NSNotification) {
+        println("onMPMoviePlayerPlaybackDidFinishNotificationReceived")
+        self.moviePlayer.view.removeFromSuperview()
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func PlayVideo(sender: UIButton) {
+        moviePlayer.view.sizeToFit()
+        self.view.addSubview(moviePlayer.view)
+        moviePlayer.fullscreen = true
+        moviePlayer.view.frame = CGRect(x: 0, y: 0, width: self.view.frame.size.width, height: self.view.frame.size.height)
+        moviePlayer.controlStyle = MPMovieControlStyle.Embedded
+
+        
+    }
 
 }
 
